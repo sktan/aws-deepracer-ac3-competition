1e01e88afda4265572ea7353ce7b8e4ff0a0070d
Steven Tan <steven.tan@bulletproof.net>
2019-11-14 11:54:27 +1100

    Did linting

--- py_reward/reward.py
+++ py_reward/reward.py
@@ -1,27 +1,27 @@
-import math
+""" Our DeepRacer training code """
+
+SPEED_THRESHOLD = 3
 def reward_function(params):
-    
+    """ Reward function for training our deep racer """
     reward = 1
     all_wheels_on_track = params['all_wheels_on_track']
     speed = params['speed']
     track_width = params['track_width']
     distance_from_center = params['distance_from_center']
-    
-    SPEED_TRESHOLD = 3
 
-    if speed < SPEED_TRESHOLD:
-        reward *=0.8
+    if speed < SPEED_THRESHOLD:
+        reward *= 0.8
 
     marker_1 = 0.1 * track_width
     marker_2 = 0.5 * track_width
 
-    if distance_from_center <= marker_1:    
+    if distance_from_center <= marker_1:
         reward *= 1.2
     elif distance_from_center <= marker_2:
-        reward *= 0.5    
+        reward *= 0.5
 
     if all_wheels_on_track:
         reward *= 1.2
-    else: 
-        reward *=0.01 
+    else:
+        reward *= 0.01
     return float(reward)
